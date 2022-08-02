43a925e72b063b33ca7b62685422d9518a198968
Xojiakbar <87039534+XojiakbarA@users.noreply.github.com>
2022-08-02 21:34:05 +0500

    edited allowed origin for deployment

--- src/main/java/com/task4/api/config/WebConfig.java
+++ src/main/java/com/task4/api/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://taskfourapp.herokuapp.com/")
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .allowCredentials(true)
